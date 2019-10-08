03ee500783d6b3096d40f464afd01ea482445f14
nitharsan21 <nimalaluxman.nitharsan.sio@gmail.com>
2019-10-08 16:57:11 +0200

    Implémentation des méthodes get et set Date

--- src/com/awa/awajaba/metier/Repas.java
+++ src/com/awa/awajaba/metier/Repas.java
@@ -28,4 +28,12 @@ public class Repas {
 	public void setNumero(int numero){
 		this.numero=numero;
 	}
+        
+    public LocalDate getDate(){
+		return this.date;
+	}
+	
+	public void setDate(LocalDate date){
+		this.date=date;
+	}
 }
